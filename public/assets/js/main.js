a7f35a29885ce324234ddaba800534eb4c2c055f
UshanShakya <usnshakya4@gmail.com>
2025-01-05 14:23:08 +0545

    CDN to static files

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,11 +1,3 @@
-/**
- * Template Name: Everest Group Pvt. Ltd.
- * Template URL: https://bootstrapmade.com/Everest Group Pvt. Ltd.-bootstrap-business-template/
- * Updated: Aug 07 2024 with Bootstrap v5.3.3
- * Author: BootstrapMade.com
- * License: https://bootstrapmade.com/license/
- */
-
 (function () {
   "use strict";
 
