80ae53c6a94e1a06d4cf0c81f65565fff47909cc
UshanShakya <usnshakya4@gmail.com>
2025-01-05 21:49:51 +0545

    make header active

--- public/assets/js/init.js
+++ public/assets/js/init.js
@@ -1,10 +1,23 @@
-async function loadLayout(file, target) {
+async function loadLayout(file, target, isHeaderContent) {
   try {
     const response = await fetch(file);
     if (!response.ok) {
       throw new Error(`Error loading ${file}: ${response.statusText}`);
     }
-    const html = await response.text();
+    let html = await response.text();
+    if (isHeaderContent) {
+      const pathname = window.location.pathname;
+      const parser = new DOMParser();
+      const doc = parser.parseFromString(html, "text/html");
+      const links = doc.querySelectorAll("#navmenu a");
+      links.forEach((link) => {
+        if (link.getAttribute("href") === pathname) {
+          debugger;
+          link.classList.add("active");
+        }
+      });
+      html = doc.body.innerHTML;
+    }
     document.querySelector(target).innerHTML = html;
     var script = document.createElement("script");
     script.src = "assets/js/main.js";
@@ -14,5 +27,5 @@ async function loadLayout(file, target) {
   }
 }
 
-loadLayout("/pages/layouts/header-layout.html", "#header-layout");
+loadLayout("/pages/layouts/header-layout.html", "#header-layout", true);
 loadLayout("/pages/layouts/footer-layout.html", "#footer-layout");
